f975c30b6b60144747cf5639758c97a8ac5a5a48
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-12 16:01:17 -0800

    NBTify Anvil Parsing + Types

--- src/world/parse.ts
+++ src/world/parse.ts
@@ -1,72 +1,73 @@
 import path from 'path'
 import * as NBT from 'nbtify'
 import {
-    NBTParser,
+    // NBTParser,
     AnvilParser,
-    type TagPayload,
-    type ListPayload,
+    // type TagPayload,
+    // type ListPayload,
 } from 'mc-anvil'
 
-type TagData<
-    T extends number,
-    N extends string,
-    D extends
-        | TagPayload
-        | TagData<any, any, any>
-        | TagData<any, any, any>[]
-        | GenericListPayload<any, any>
-> = {
-    type: T
-    name: N
-    data: D
-}
+// type TagData<
+//     T extends number,
+//     N extends string,
+//     D extends
+//         | TagPayload
+//         | TagData<any, any, any>
+//         | TagData<any, any, any>[]
+//         | GenericListPayload<any, any>
+// > = {
+//     type: T
+//     name: N
+//     data: D
+// }
 
-type PalettedContainer = {
-    type: NBT.TAG.LIST
-    name: 'palette'
-    data: ListPayload
-}
+// type PalettedContainer = {
+//     type: NBT.TAG.LIST
+//     name: 'palette'
+//     data: ListPayload
+// }
 
-type GenericListPayload<
-    T extends number,
-    D extends TagPayload | TagData<any, any, any>[]
-> = {
-    subType: T
-    data: D[]
-}
+// type GenericListPayload<
+//     T extends number,
+//     D extends TagPayload | TagData<any, any, any>[]
+// > = {
+//     subType: T
+//     data: D[]
+// }
 
-type ChunkSection =
-    | TagData<NBT.TAG.COMPOUND, 'block_states', PalettedContainer[]>
-    | TagData<NBT.TAG.COMPOUND, 'biomes', PalettedContainer[]>
-    | TagData<NBT.TAG.BYTE, 'BlockLight', number[]>
-    | TagData<NBT.TAG.BYTE, 'SkyLight', number[]>
-    | TagData<NBT.TAG.BYTE, 'Y', number>
+// type ChunkSection =
+//     | TagData<NBT.TAG.COMPOUND, 'block_states', PalettedContainer[]>
+//     | TagData<NBT.TAG.COMPOUND, 'biomes', PalettedContainer[]>
+//     | TagData<NBT.TAG.BYTE, 'BlockLight', number[]>
+//     | TagData<NBT.TAG.BYTE, 'SkyLight', number[]>
+//     | TagData<NBT.TAG.BYTE, 'Y', number>
 
-type ChunkData =
-    | TagData<NBT.TAG.STRING, 'Status', string>
-    | TagData<NBT.TAG.INT, 'zPos', number>
-    | TagData<NBT.TAG.LIST, 'block_entities', ListPayload>
-    | TagData<NBT.TAG.INT, 'yPos', number>
-    | TagData<NBT.TAG.LONG, 'LastUpdate', bigint>
-    | TagData<NBT.TAG.COMPOUND, 'structures', any[]>
-    | TagData<NBT.TAG.LONG, 'InhabitedTime', bigint>
-    | TagData<NBT.TAG.INT, 'xPos', number>
-    | TagData<NBT.TAG.COMPOUND, 'Heightmaps', any[]>
-    | TagData<
-          NBT.TAG.LIST,
-          'sections',
-          GenericListPayload<NBT.TAG.COMPOUND, ChunkSection[]>
-      >
-    | TagData<NBT.TAG.BYTE, 'isLightOn', 0 | 1>
-    | TagData<NBT.TAG.LIST, 'block_ticks', ListPayload>
-    | TagData<NBT.TAG.LIST, 'PostProcessing', ListPayload>
-    | TagData<NBT.TAG.BYTE, 'DataVersion', number>
-    | TagData<NBT.TAG.LIST, 'fluid_ticks', ListPayload>
-    | TagData<NBT.TAG.COMPOUND, 'CarvingMasks', any[]>
+// type ChunkData =
+//     | TagData<NBT.TAG.STRING, 'Status', string>
+//     | TagData<NBT.TAG.INT, 'zPos', number>
+//     | TagData<NBT.TAG.LIST, 'block_entities', ListPayload>
+//     | TagData<NBT.TAG.INT, 'yPos', number>
+//     | TagData<NBT.TAG.LONG, 'LastUpdate', bigint>
+//     | TagData<NBT.TAG.COMPOUND, 'structures', any[]>
+//     | TagData<NBT.TAG.LONG, 'InhabitedTime', bigint>
+//     | TagData<NBT.TAG.INT, 'xPos', number>
+//     | TagData<NBT.TAG.COMPOUND, 'Heightmaps', any[]>
+//     | TagData<
+//           NBT.TAG.LIST,
+//           'sections',
+//           GenericListPayload<NBT.TAG.COMPOUND, ChunkSection[]>
+//       >
+//     | TagData<NBT.TAG.BYTE, 'isLightOn', 0 | 1>
+//     | TagData<NBT.TAG.LIST, 'block_ticks', ListPayload>
+//     | TagData<NBT.TAG.LIST, 'PostProcessing', ListPayload>
+//     | TagData<NBT.TAG.BYTE, 'DataVersion', number>
+//     | TagData<NBT.TAG.LIST, 'fluid_ticks', ListPayload>
+//     | TagData<NBT.TAG.COMPOUND, 'CarvingMasks', any[]>
 
-type ChunkTag = TagData<NBT.TAG.COMPOUND, '', ChunkData[]>
+// type ChunkTag = TagData<NBT.TAG.COMPOUND, '', ChunkData[]>
 
 import { readdir } from 'node:fs/promises'
+import type { Chunk } from 'region-types'
 
 // read all the files in the current directory
 const regions = await readdir(path.join(import.meta.dir, 'region'))
@@ -77,6 +78,13 @@ const coords = regions.map((region) => {
 console.log(coords)
 
 const formatCoords = (x: number, z: number) => `${x}.${z}`
+
+const chunkNBTFormat = {
+    rootName: "",
+    endian: "big",
+    compression: null,
+    bedrockLevel: null
+} as const satisfies NBT.Format;
 
 const loadTerrain = async (filename: string) => {
     const file = Bun.file(path.join(import.meta.dir, 'region', filename))
@@ -89,7 +97,7 @@ const loadTerrain = async (filename: string) => {
         if (b === undefined) return acc
         return Buffer.concat([acc, Buffer.from(b)])
     }, Buffer.from([]))
-    const tag = new NBTParser(data.buffer as ArrayBuffer).getTag()
+    const tag = await NBT.read<Chunk>(data.buffer,{ ...chunkNBTFormat, strict: false })
     console.log(tag)
     return data
 }
